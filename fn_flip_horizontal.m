08e7ce49925d4848f56cc8184ffd15daefbaa174
kurniawanhandito <kurniawanhandito@gmail.com>
2017-10-23 21:23:10 +0700

    fix function

--- fn_flip_horizontal.m
+++ fn_flip_horizontal.m
@@ -1,15 +1,6 @@
 function imgFlip = fn_flip_horizontal(img)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
-    [row,col,page]=size(img);
-    for x=1:page
-        k=col;
-        for i=1:row
-            for j=1:col
-                imgFlip(i,k,x)=img(i,j,x);
-            end
-            k=k-1;
-        end
-    end
+    imgFlip=img(1:end,end:-1:1,:);
 end
 
