07f875895eda7eda3f989c1692bb9ca07c2f49f4
kurniawanhandito <kurniawanhandito@gmail.com>
2017-10-23 19:07:46 +0700

    add function resolution

---
+++ fn_resolution.m
@@ -0,0 +1,9 @@
+function resolution = fn_resolution(img)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+    [row,col,page]=size(img);
+    resRow=int2str(row);
+    resCol=int2str(col);
+    resolution=strcat(resRow,' x ',resCol);
+end
+
