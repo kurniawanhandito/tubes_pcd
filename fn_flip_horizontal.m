6932f367ea6f961e5e16bf3b0bc0a94a799a57fd
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-02 11:25:20 +0700

    line changed

--- fn_flip_horizontal.m
+++ fn_flip_horizontal.m
@@ -1,6 +1,5 @@
 function imgFlip = fn_flip_horizontal(img)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
-    imgFlip=img(1:end,end:-1:1,:);
+    imgFlip=img(1:end,end:-1:1,:); 
 end
-
