46e00fe9e537d66ffd776e5cd362c4defbbe20e7
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-04 00:33:10 +0700

    add init function oh histogram

---
+++ fn_hist.m
@@ -0,0 +1,13 @@
+function fn_hist(img)
+%fn_hist Summary of this function goes here
+%   Detailed explanation goes here
+[row,col,page] = size(img);
+histRed=zeros(1,256);
+histBlue=zeros(1,256);
+histGreen=zeros(1,256);
+for 
+    for
+        for
+        end
+    end
+end
