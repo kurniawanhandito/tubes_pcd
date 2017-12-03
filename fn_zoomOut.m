f261d08856ca50be74f53dc9d05908cbc9f0b81e
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-03 10:14:32 +0700

    add function zoom out

---
+++ fn_zoomOut.m
@@ -0,0 +1,12 @@
+function imgZoom = fn_zoomOut(img, times)
+%fn_zoomOut Summary of this function goes here
+%   Detailed explanation goes here
+[row, col, page] = size(img);
+
+for i=1:page
+    for j=1:row*times
+        for k=1:col*times
+            imgZoom(j,k,i)=img(ceil(j/times),ceil(k/times),i);
+        end
+    end
+end
