5c06c132732524492a80287d473aa1bda03af849
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-03 21:52:59 +0700

    fix function horizontal flipping

--- fn_flipH.m
+++ fn_flipH.m
@@ -1,6 +1,15 @@
 function imgFlip = fn_flipH(img)
 %fn_flipH Summary of this function goes here
 %   Detailed explanation goes here
-
+[row,col,page]=size(img);
+for p=1:page
+    for i=1:row
+        k=col;
+        for j=1:col
+            imgFlip(i,k,p)=img(i,j,p);
+            k=k-1;
+        end
+    end
+end
 end
 
