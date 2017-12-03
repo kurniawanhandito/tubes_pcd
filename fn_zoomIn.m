162ebe8e1147599fa8e85e9a1c6a22687914bde4
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-03 20:24:46 +0700

    fix line code

--- fn_zoomIn.m
+++ fn_zoomIn.m
@@ -2,7 +2,6 @@ function imgZoom = fn_zoomIn(img, times)
 %fn_zoomIn Summary of this function goes here
 %   Detailed explanation goes here
 [row, col, page] = size(img);
-
 for i=1:page
     for j=1:row*times
         for k=1:col*times
