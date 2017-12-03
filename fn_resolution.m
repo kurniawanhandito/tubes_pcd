f8044d637c8922917d88efe71747ed649302e61a
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-03 10:09:21 +0700

    fix desc

--- fn_resolution.m
+++ fn_resolution.m
@@ -1,8 +1,8 @@
 function resolution = fn_resolution(img)
-%UNTITLED6 Summary of this function goes here
+%fn_resolution Summary of this function goes here
 %   Detailed explanation goes here
-    [row,col,page]=size(img);
-    resRow=int2str(row);
-    resCol=int2str(col);
-    resolution=strcat(resRow,' x ',resCol);
+[row,col,page]=size(img);
+resRow=int2str(row);
+resCol=int2str(col);
+resolution=strcat(resRow,' x ',resCol);
 end
