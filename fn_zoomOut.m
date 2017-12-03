15f29e3c06b38293068363c90938d4f31c076653
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-03 20:24:56 +0700

    fix line code

--- fn_zoomOut.m
+++ fn_zoomOut.m
@@ -2,7 +2,6 @@ function imgZoom = fn_zoomOut(img, times)
 %fn_zoomOut Summary of this function goes here
 %   Detailed explanation goes here
 [row, col, page] = size(img);
-
 for i=1:page
     for j=1:row*times
         for k=1:col*times
