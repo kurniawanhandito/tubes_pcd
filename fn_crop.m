fe3dfc7fb7e7af7df6e2efe791d2b40d86f446f7
kurniawanhandito <kurniawanhandito@gmail.com>
2017-12-04 09:46:37 +0700

    fix crop function

--- fn_crop.m
+++ fn_crop.m
@@ -1,6 +1,15 @@
-function imgCrop = fn_crop(img,sx,sy,fx,fy)
-%UNTITLED Summary of this function goes here
+function imgCrop = fn_crop(img,x1,y1,width,height)
+%fn_crop Summary of this function goes here
 %   Detailed explanation goes here
 [row,col,page]=size(img);
-
+i=1;
+j=1;
+for p=1:page
+    for x1=x1:height
+        for y1=y1:width
+            imgCrop(i,j,p)=img(x1,y1,p);
+            j=j+1
+        end
+        i=i+1
+    end
 end
